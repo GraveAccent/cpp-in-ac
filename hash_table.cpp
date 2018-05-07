bbf46336b4a69ba0b227b70407a3322328346ff3
GraveAccent <thegraveac@gmail.com>
2018-05-07 12:05:13 +0800

    added hash_table.cpp written by 里克

---
+++ hash_table.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char charr[27];
+    char ch, gh;
+    int i, k, arr[26] = {0};
+
+    for(gh = 'a', i = 0; gh <= 'z'; gh++, i++)
+        charr[i] = gh;
+    while(1)
+    {
+        while(1)
+        {
+        //    cin.get(ch);
+            ch = cin.get();
+            if (ch == '\n')
+                break;
+            else if (!cin)
+                return 0;
+            if (ch >= 'a' && ch <= 'z')
+                arr[ch - 'a']++;
+        }
+
+        if (!cin)
+            return 0;
+        for (i = 1; i < 26; i++)
+        {
+            cout << charr[i] << ":" << arr[i] << endl;
+            arr[i] = 0;
+        }
+        cout << endl;
+    }
+}
